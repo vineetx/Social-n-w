4f4116f1d33a6bf735c61f9c66dc87e921ec45f7
vineetx <vineetj_29@yahoo.com>
2017-04-19 18:32:29 +0530

    profile made

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
 		@post = Post.new
 		@posts = Post.all.order("created_at DESC")
 	end
+
+	def profile
+
+		@user = User.find_by(id: params[:format])
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
  
   root 'home#index'
   devise_for :users, :controllers => {:registrations => "registrations"}
+  get 'profile', to: 'home#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
