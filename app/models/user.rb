c7460930a3acf7a4dcb5444480a59cdf71da40ed
vineetx <vineetj_29@yahoo.com>
2017-04-19 15:42:41 +0530

    posts and devise done

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+         has_many :posts
+
+         has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  		validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+end
